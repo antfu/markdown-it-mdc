e1cd58745563ade3e85b593a76bb6184e627be74
Anthony Fu <anthonyfu117@hotmail.com>
2023-08-29 10:04:40 +0200

    feat: inline-component

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,17 @@
 import type MarkdownIt from 'markdown-it'
 import { MarkdownItMdcBlock } from './block'
 import { MarkdownItInlineProps } from './inline-props'
+import { MarkdownItInlineComponent } from './inline-component'
 
 export interface MarkdownItMdcOptions {
 
 }
 
-const MarkdownItMdc: MarkdownIt.PluginWithOptions<MarkdownItMdcOptions> = (
-  md,
-  _options = {},
-) => {
+const MarkdownItMdc: MarkdownIt.PluginWithOptions<MarkdownItMdcOptions> = (md, _options = {}) => {
   md
     .use(MarkdownItMdcBlock)
     .use(MarkdownItInlineProps)
+    .use(MarkdownItInlineComponent)
 }
 
 export default MarkdownItMdc
---
+++ src/inline-component.ts
@@ -0,0 +1,65 @@
+import { text } from 'node:stream/consumers'
+import type MarkdownIt from 'markdown-it'
+import Token from 'markdown-it/lib/token'
+import { s } from 'vitest/dist/reporters-2ff87305'
+import { searchProps } from './parse/props'
+
+export interface MdcInlineComponentOptions {
+
+}
+
+export const MarkdownItInlineComponent: MarkdownIt.PluginWithOptions<MdcInlineComponentOptions> = (md) => {
+  md.inline.ruler.after('entity', 'mdc_inline_component', (state, silent) => {
+    const start = state.pos
+    const char = state.src[start]
+
+    if (char !== ':')
+      return false
+
+    let index = start + 1
+    let contentStart = -1
+    let contentEnd = -1
+    while (index < state.src.length) {
+      const char = state.src[index]
+      if (char === '[') {
+        contentStart = index + 1
+        while (index < state.src.length) {
+          index += 1
+          if (state.src[index] === ']') {
+            contentEnd = index
+            index += 1
+            break
+          }
+        }
+        break
+      }
+      if (!/[\w\$_-]/.test(char))
+        break
+      index += 1
+    }
+
+    if (contentEnd !== contentStart) {
+      const name = state.src.slice(start + 1, contentStart - 1)
+      const body = state.src.slice(contentStart, contentEnd)
+      state.posMax = contentStart
+      state.push('mdc_inline_component', name, 1)
+      state.pos = contentStart
+      state.posMax = contentEnd
+      const text = state.push('text', '', 0)
+      text.content = body
+      state.pos = contentEnd
+      state.posMax = contentEnd
+      state.push('mdc_inline_component', name, -1)
+    }
+    else {
+      state.posMax = index
+      const name = state.src.slice(start + 1, index)
+      state.push('mdc_inline_component', name, 0)
+    }
+
+    state.pos = index
+    state.posMax = index
+
+    return true
+  })
+}
