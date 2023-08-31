21d57d3fe30b01def5725ce10b36224f460cfee7
Anthony Fu <anthonyfu117@hotmail.com>
2023-08-31 21:08:53 +0200

    feat: options to opt-out each syntax

--- src/index.ts
+++ src/index.ts
@@ -3,19 +3,62 @@ import { MarkdownItMdcBlock } from './syntax/block'
 import { MarkdownItInlineProps } from './syntax/inline-props'
 import { MarkdownItInlineComponent } from './syntax/inline-component'
 import { MarkdownItInlineSpan } from './syntax/inline-span'
-import { MarkdownItMdcBlockContent } from './syntax/block-content'
 
 export interface MarkdownItMdcOptions {
-
+  /**
+   * Options for toggling each syntax feature.
+   */
+  syntax?: {
+    /**
+     * Enable block component syntax.
+     *
+     * @see https://content.nuxtjs.org/guide/writing/mdc#block-components
+     * @default true
+     */
+    blockComponent?: boolean
+    /**
+     * Enable inline props syntax.
+     *
+     * @see https://content.nuxtjs.org/guide/writing/mdc#inline-props
+     * @default true
+     */
+    inlineProps?: boolean
+    /**
+     * Enable inline span syntax.
+     *
+     * @see https://content.nuxtjs.org/guide/writing/mdc#inline-span
+     * @default true
+     */
+    inlineSpan?: boolean
+    /**
+     * Enable inline component syntax.
+     *
+     * @see https://content.nuxtjs.org/guide/writing/mdc#inline-component
+     * @default true
+     **/
+    inlineComponent?: boolean
+  }
 }
 
-const MarkdownItMdc: MarkdownIt.PluginWithOptions<MarkdownItMdcOptions> = (md, _options = {}) => {
-  md
-    .use(MarkdownItMdcBlock)
-    .use(MarkdownItInlineProps)
-    .use(MarkdownItInlineComponent)
-    .use(MarkdownItInlineSpan)
-    .use(MarkdownItMdcBlockContent)
+const MarkdownItMdc: MarkdownIt.PluginWithOptions<MarkdownItMdcOptions> = (md, options = {}) => {
+  const {
+    blockComponent = true,
+    inlineProps = true,
+    inlineSpan = true,
+    inlineComponent = true,
+  } = options.syntax || {}
+
+  if (blockComponent)
+    md.use(MarkdownItMdcBlock)
+
+  if (inlineProps)
+    md.use(MarkdownItInlineProps)
+
+  if (inlineSpan)
+    md.use(MarkdownItInlineSpan)
+
+  if (inlineComponent)
+    md.use(MarkdownItInlineComponent)
 }
 
 export default MarkdownItMdc
--- src/syntax/block.ts
+++ src/syntax/block.ts
@@ -1,5 +1,6 @@
 import type MarkdownIt from 'markdown-it'
 import type Token from 'markdown-it/lib/token'
+import YAML from 'js-yaml'
 import { parseBlockParams } from '../parse/block-params'
 
 export const MarkdownItMdcBlock: MarkdownIt.PluginSimple = (md) => {
@@ -179,6 +180,108 @@ export const MarkdownItMdcBlock: MarkdownIt.PluginSimple = (md) => {
     },
     {
       alt: ['paragraph', 'reference', 'blockquote', 'list'],
+    },
+  )
+
+  md.block.ruler.after('code', 'mdc_block_yaml',
+  // eslint-disable-next-line prefer-arrow-callback
+    function mdc_block_yaml(state, startLine, endLine, silent) {
+      if (!state.env.mdcBlockTokens?.length)
+        return false
+
+      const start = state.bMarks[startLine] + state.tShift[startLine]
+      const end = state.eMarks[startLine]
+
+      if (state.src.slice(start, end) !== '---')
+        return false
+
+      let lineEnd = startLine + 1
+
+      let found = false
+      while (lineEnd < endLine) {
+        const line = state.src.slice(state.bMarks[lineEnd] + state.tShift[startLine], state.eMarks[lineEnd])
+        if (line === '---') {
+          found = true
+          break
+        }
+        lineEnd += 1
+      }
+
+      if (!found)
+        return false
+
+      if (!silent) {
+        const yaml = state.src.slice(state.bMarks[startLine + 1], state.eMarks[lineEnd - 1])
+
+        const data = YAML.load(yaml) as Record<string, unknown>
+        const token = state.env.mdcBlockTokens[0]
+        Object.entries(data).forEach(([key, value]) => {
+          if (key === 'class')
+            token.attrJoin(key, value)
+          else
+            token.attrSet(key, typeof value === 'string' ? value : JSON.stringify(value))
+        })
+      }
+
+      state.line = lineEnd + 1
+      state.lineMax = lineEnd + 1
+      return true
+    })
+
+  md.block.ruler.after('code', 'mdc_block_slots',
+  // eslint-disable-next-line prefer-arrow-callback
+    function mdc_block(state, startLine, endLine, silent) {
+      if (!state.env.mdcBlockTokens?.length)
+        return false
+
+      const start = state.bMarks[startLine] + state.tShift[startLine]
+
+      if (!(state.src[start] === '#' && state.src[start + 1] !== ' '))
+        return false
+
+      const line = state.src.slice(start, state.eMarks[startLine])
+
+      const {
+        name,
+        props,
+      } = parseBlockParams(line.slice(1))
+
+      let lineEnd = startLine + 1
+
+      while (lineEnd < endLine) {
+        const line = state.src.slice(state.bMarks[lineEnd] + state.tShift[startLine], state.eMarks[lineEnd])
+        if (line.match(/^#(\w)+/) || line.startsWith('::'))
+          break
+        lineEnd += 1
+      }
+
+      if (silent) {
+        state.line = lineEnd
+        state.lineMax = lineEnd
+        return true
+      }
+
+      state.lineMax = startLine + 1
+      const slot = state.push('mdc_block_slot', 'template', 1)
+      slot.attrSet(`#${name}`, '')
+      props?.forEach(([key, value]) => {
+        if (key === 'class')
+          slot.attrJoin(key, value)
+        else
+          slot.attrSet(key, value)
+      })
+
+      state.line = startLine + 1
+      state.lineMax = lineEnd
+
+      state.md.block.tokenize(state, startLine + 1, lineEnd)
+
+      state.push('mdc_block_slot', 'template', -1)
+
+      state.line = lineEnd
+      state.lineMax = lineEnd
+
+      return true
     },
   )
 }
