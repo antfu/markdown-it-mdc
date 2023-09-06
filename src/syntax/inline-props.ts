1e37c92757ac80d3cb5e48a6c649efb252f9034a
Anthony Fu <anthonyfu117@hotmail.com>
2023-09-06 17:26:59 +0200

    feat: improve list handling
    https://github.com/antfu/markdown-it-mdc/issues/1#issuecomment-1708531564

--- src/syntax/inline-props.ts
+++ src/syntax/inline-props.ts
@@ -52,17 +52,22 @@ export const MarkdownItInlineProps: MarkdownIt.PluginWithOptions<MdcInlinePropsO
     return ''
   }
 
-  // Apply the props to the parent token
-  // when it's the only child of the inline token inside a paragraph or heading
-  // e.g. `# Heading {class="foo"}`, it will apply the class to the `h1` tag instead of creating a `span` tag
   const _parse = md.parse
   md.parse = function (src, env) {
     const tokens = _parse.call(this, src, env)
 
+    // Apply the props to the parent token
+    // when it's the only child of the inline token inside a paragraph or heading
+    // e.g. `# Heading {class="foo"}`, it will apply the class to the `h1` tag instead of creating a `span` tag
     tokens.forEach((token, index) => {
       const prev = tokens[index - 1]
-      if (prev?.type !== 'heading_open' && prev?.type !== 'paragraph_open')
+      const next = tokens[index + 1]
+      if (!['heading_open', 'paragraph_open', 'list_item_open'].includes(prev?.type) || prev.hidden)
         return
+
+      // list item handling
+      if (token.hidden && next.type === 'inline')
+        token = next
 
       if (
         token.type === 'inline'
@@ -78,6 +83,38 @@ export const MarkdownItInlineProps: MarkdownIt.PluginWithOptions<MdcInlinePropsO
           else
             prev.attrSet(key, value)
         })
+      }
+    })
+
+    // When using `::ul` syntax, to wrap with a list,
+    // we deduplicate the `ul` tag and hide the original one
+    // when it's exactly the only child of the `mdc_block_open` token
+    tokens.forEach((tokenOpen, index) => {
+      if (tokenOpen.type !== 'bullet_list_open')
+        return
+
+      const prev = tokens[index - 1]
+      if (prev.type !== 'mdc_block_open' || prev.tag !== 'ul')
+        return
+
+      // find the matching close token
+      let closeIndex = index + 1
+      while (closeIndex < tokens.length) {
+        const close = tokens[closeIndex]
+        if (close.type === 'bullet_list_close' && close.level === tokenOpen.level)
+          break
+        closeIndex += 1
+      }
+      const tokenClose = tokens[closeIndex]
+      if (tokenClose.type !== 'bullet_list_close')
+        return
+
+      // when prev and next are both `mdc_block` and `ul`,
+      // we hide the original `ul` token
+      const next = tokens[closeIndex + 1]
+      if (next.type === 'mdc_block_close' && next.tag === 'ul') {
+        tokenOpen.hidden = true
+        tokenClose.hidden = true
       }
     })
 
@@ -126,6 +163,8 @@ export const MarkdownItInlineProps: MarkdownIt.PluginWithOptions<MdcInlinePropsO
 
         if (prev.nesting === -1)
           throw new Error(`No matching opening tag found for ${JSON.stringify(prev)}`)
+
+        // console.log('apply', token.attrs, 'to', prev)
 
         token.attrs?.forEach(([key, value]) => {
           if (key === 'class')
